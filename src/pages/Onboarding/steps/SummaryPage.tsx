af97268b9b366764ee3fae7342f9cb4991cb3d0d
Karel Durdis <kareldurdis@users.noreply.github.com>
2020-10-31 20:17:31 +0100

    Fake data (#16)
    * Transactions (WIP)
    
    * Use fake spendings in Monthly payments
    
    * Use getSpendings()
    
    * Use sumIncomes in Summary
    
    * Use sumIncomes in Summary
    
    * Add fake dates to transactions

--- src/pages/Onboarding/steps/SummaryPage.tsx
+++ src/pages/Onboarding/steps/SummaryPage.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import Content from "../../../components/Content";
-import EmergencyFundLogo from "../../../assets/emergency-fund.jpg";
 import {Link} from "react-router-dom";
 import {Routes} from "../../../components/Router/routes";
 import {createUseStyles} from "react-jss";
-import {formatEuro} from '../../../utils';
+import {formatEuro, sumIncomes} from '../../../utils';
 
 const useStyles = createUseStyles({
     card: {
@@ -38,7 +37,7 @@ const SpendCard = ({name, amount, monthly}: Props) => {
 }
 
 const SummaryPage = () => {
-    const income = 300;
+    const income = sumIncomes();
     const payments = {
         monthly: 50,
         dreams: 200,
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,7 @@
+import transactions from "./__mocks__/transactions";
+
 export const formatEuro = (amount: number) => new Intl.NumberFormat('en', {style: 'currency', currency: 'EUR'}).format(amount);
+
+export const getSpendings = () => transactions.filter((transaction) => transaction.amount < 0);
+export const getIncomes = () => transactions.filter((transaction) => transaction.amount > 0);
+export const sumIncomes = () => getIncomes().reduce((sum, transaction) => sum + transaction.amount, 0);
