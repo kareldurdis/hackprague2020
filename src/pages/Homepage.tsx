5e5522f60c5d3c0304321b6aca36c272f62a7b57
Lukáš Havlíček <info@lukas-havlicek.cz>
2020-10-31 12:10:52 +0100

    Intro page basics

--- src/pages/Homepage.tsx
+++
@@ -1,8 +0,0 @@
-import React, { memo } from "react";
-import Content from "../components/Content";
-
-const Homepage = () => {
-  return <Content>Homepage</Content>;
-};
-
-export default memo(Homepage);
