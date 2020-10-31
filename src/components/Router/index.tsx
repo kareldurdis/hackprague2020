f5caed93c7deb61fa036f43f77ba525582d6b051
Lukáš Havlíček <info@lukas-havlicek.cz>
2020-10-31 14:18:35 +0100

    Homepage with onboarding redirect

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,14 +1,21 @@
 import React, { memo } from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import { Routes } from "./routes";
+import Onboarding from "../../pages/Onboarding";
 import Homepage from "../../pages/Homepage";
+import { DreamsContextProvider } from "../../context/dreams";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path={Routes.Homepage} exact>
-          <Homepage />
+          <DreamsContextProvider>
+            <Homepage />
+          </DreamsContextProvider>
+        </Route>
+        <Route path={Routes.Onboarding}>
+          <Onboarding />
         </Route>
       </Switch>
     </BrowserRouter>
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,14 +1,25 @@
 import React, { memo } from "react";
-import { Button } from "reakit";
+import { Redirect } from "react-router-dom";
 import Content from "../components/Content";
+import { Routes } from "../components/Router/routes";
+import { useDreamsContext } from "../context/dreams";
 
 const Homepage = () => {
+  const { dreams } = useDreamsContext();
+
+  if (!dreams.length) {
+    return (
+      <Redirect
+        to={{
+          pathname: Routes.Onboarding,
+        }}
+      />
+    );
+  }
+
   return (
     <Content>
-      <h1>Dream Assistant</h1>
-
-      {/* TODO: Existing dreams list */}
-      <Button>Start dreaming</Button>
+      <h1>Dashboard</h1>
     </Content>
   );
 };
