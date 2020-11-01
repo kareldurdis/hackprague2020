6ff1c745bfa2e0233f5482e6f3afcee71f7643f6
Karel Durdis <karel.durdis@gmail.com>
2020-11-01 09:20:12 +0100

    Remove search

--- src/components/PickTransactions.tsx
+++ src/components/PickTransactions.tsx
@@ -1,7 +1,5 @@
 import { Checkbox, useCheckboxState } from 'reakit/Checkbox';
-import React, { useEffect, useState } from 'react';
-import Content from './Content';
-import { Input } from 'reakit/Input';
+import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { formatEuro, getSpendings } from '../utils';
 import { Transaction } from '../__mocks__/transactions';
@@ -48,12 +46,6 @@ interface Props {
 export const PickTransactions = ({ onPick }: Props) => {
   const classes = useStyles();
   const checkbox = useCheckboxState({ state: [] });
-  const [showPopup, setPopupVisibility] = useState(false);
-  const [search, setSearch] = useState('');
-
-  useEffect(() => {
-    setSearch('');
-  }, [showPopup]);
 
   // Get only negative transactions
   const spendings = getSpendings();
@@ -68,31 +60,9 @@ export const PickTransactions = ({ onPick }: Props) => {
 
   return (
     <>
-      <div>
-        <h2>Choose transactions</h2>
-        <ul>
-          {picked.map((transaction) => {
-            return (
-              <li key={transaction.id}>
-                {formatLabel(transaction)}
-                <button
-                  onClick={() => {
-                    checkbox.setState((value) =>
-                      (value as string[]).filter((item) => item !== transaction.id)
-                    );
-                  }}
-                >
-                  Remove
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-
-      <h2>Suggestions</h2>
+      <h2>Choose transactions</h2>
       <ul className={classes.ul}>
-        {spendings.slice(0, 5).map((transaction) => {
+        {spendings.map((transaction) => {
           return (
             <li key={transaction.id}>
               <label>
@@ -103,38 +73,8 @@ export const PickTransactions = ({ onPick }: Props) => {
           );
         })}
       </ul>
-      <button onClick={() => setPopupVisibility(true)}>More</button>
 
       <button onClick={() => onPick(picked)}>Continue</button>
-
-      {showPopup ? (
-        <div className={classes.popup}>
-          <Content>
-            <Input
-              placeholder="Hledat"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-
-            <ul className={classes.ul}>
-              {(search
-                ? spendings.filter((t) => t.payee.toLowerCase().includes(search.toLowerCase()))
-                : spendings
-              ).map((transaction) => {
-                return (
-                  <li key={transaction.id}>
-                    <label>
-                      <Checkbox {...checkbox} value={transaction.id} />
-                      {formatLabel(transaction)}
-                    </label>
-                  </li>
-                );
-              })}
-            </ul>
-            <button onClick={() => setPopupVisibility(false)}>Done</button>
-          </Content>
-        </div>
-      ) : null}
     </>
   );
 };
