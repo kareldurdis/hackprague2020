1aa34f69296c7f76acaa314b4b45f1290bc8b093
Lukáš Havlíček <info@lukas-havlicek.cz>
2020-11-01 10:08:24 +0100

    Move payments to context

--- src/context/dreams.tsx
+++ src/context/dreams.tsx
@@ -11,12 +11,27 @@ export interface Dream {
   image?: string;
 }
 
+export enum PaymentType {
+  monthly = 'monthly',
+  annual = 'annual',
+}
+
+export interface Payment {
+  id: string;
+  name: string;
+  type: PaymentType;
+  cost: number;
+}
+
 export interface DreamsContextValue {
   dreams: Dream[];
   addNewDream: (dream: Dream) => void;
   setDreams: (dreams: Dream[]) => void;
   onboarded: boolean;
   setIsOnboarded: (onboarded: boolean) => void;
+  payments: Payment[];
+  addPayment: (payment: Payment) => void;
+  setPayments: (payments: Payment[]) => void;
 }
 
 const DreamsContext = createContext<DreamsContextValue>({
@@ -25,6 +40,9 @@ const DreamsContext = createContext<DreamsContextValue>({
   onboarded: false,
   setIsOnboarded: () => {},
   setDreams: () => {},
+  payments: [],
+  addPayment: () => {},
+  setPayments: () => {},
 });
 
 interface DreamsContextProviderProps {}
@@ -32,14 +50,29 @@ interface DreamsContextProviderProps {}
 export const DreamsContextProvider = memo(
   ({ children }: PropsWithChildren<DreamsContextProviderProps>) => {
     const [dreams, setDreams] = useStorage('dreams', dreamsMock);
+    const [payments, setPayments] = useStorage<Payment[]>('payments', []);
     const [onboarded, setIsOnboarded] = useStorage('onboarded', false);
 
     const addNewDream = (dream: Dream) => {
       return setDreams((state) => [...(state || []), dream]);
     };
+    const addPayment = (payment: Payment) => {
+      return setPayments((state) => [...(state || []), payment]);
+    };
 
     return (
-      <DreamsContext.Provider value={{ dreams, addNewDream, onboarded, setIsOnboarded, setDreams }}>
+      <DreamsContext.Provider
+        value={{
+          dreams,
+          addNewDream,
+          onboarded,
+          setIsOnboarded,
+          setDreams,
+          payments,
+          addPayment,
+          setPayments,
+        }}
+      >
         {children}
       </DreamsContext.Provider>
     );
--- src/pages/Onboarding/steps/MontlyPayments.tsx
+++ src/pages/Onboarding/steps/MontlyPayments.tsx
@@ -4,15 +4,23 @@ import { PickTransactions } from '../../../components/PickTransactions';
 import { Routes } from '../../../components/Router/routes';
 import { useLocalStorage } from 'react-use';
 import { Transaction } from '../../../__mocks__/transactions';
+import { PaymentType, useDreamsContext } from '../../../context/dreams';
 
 const MontlyPayments = () => {
   const history = useHistory();
-  const [, setExpenses] = useLocalStorage<Transaction[]>('monthly-expenses');
+  const { setPayments } = useDreamsContext();
   return (
     <PickTransactions
       title={'Monthly'}
-      onPick={(transactions) => {
-        setExpenses(transactions);
+      onPick={(transactions: Transaction[]) => {
+        setPayments(
+          transactions.map((transaction) => ({
+            id: transaction.id,
+            name: `${transaction.payee}: ${transaction.description}`,
+            cost: transaction.amount,
+            type: PaymentType.monthly,
+          }))
+        );
         history.push(Routes.Annual_Payments);
       }}
     />
